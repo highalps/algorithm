01d4d8affa28d819dc21f6af542a63b17aa8dfc9
highalps <highalps93@gmail.com>
2021-04-22 03:16:03 +0900

    refactor: Reformat cpp code as Google style guide

--- baekjoon/11089.cpp
+++ baekjoon/11089.cpp
@@ -1,12 +1,13 @@
+#include <limits.h>
+#include <memory.h>
 #include <stdio.h>
 #include <string.h>
-#include <vector>
-#include <limits.h>
+
+#include <algorithm>
+#include <map>
 #include <queue>
 #include <stack>
-#include <map>
-#include <algorithm>
-#include <memory.h>
+#include <vector>
 using namespace std;
 #define ll long long
 
@@ -14,27 +15,30 @@ int a[2][41];
 ll dp[2][801][801];
 const int w = 400;
 bool tog;
-int main()
-{
-	int n;
-	int as = 0, _as = 0, bs = 0, _bs = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d %d", &a[0][i], &a[1][i]);
-		if (a[0][i] >= 0)as += a[0][i];
-		else _as -= a[0][i];
-		if (a[1][i] >= 0)bs += a[1][i];
-		else _bs -= a[1][i];
-	}
-	dp[tog][w][w] = 1;
-	for (int k = 0; k < n; k++) {
-		tog = !tog;
-		memset(dp[tog], 0, sizeof(dp[tog]));
-		for (int i = w + as; i >= w - _as; i--) {
-			for (int j = w + bs; j >= w - _bs; j--) {
-				dp[tog][i][j] += dp[!tog][i - a[0][k]][j - a[1][k]] + dp[!tog][i][j];
-			}
-		}
-	}
-	printf("%lld\n", dp[tog][w][w] - 1);
+int main() {
+  int n;
+  int as = 0, _as = 0, bs = 0, _bs = 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d", &a[0][i], &a[1][i]);
+    if (a[0][i] >= 0)
+      as += a[0][i];
+    else
+      _as -= a[0][i];
+    if (a[1][i] >= 0)
+      bs += a[1][i];
+    else
+      _bs -= a[1][i];
+  }
+  dp[tog][w][w] = 1;
+  for (int k = 0; k < n; k++) {
+    tog = !tog;
+    memset(dp[tog], 0, sizeof(dp[tog]));
+    for (int i = w + as; i >= w - _as; i--) {
+      for (int j = w + bs; j >= w - _bs; j--) {
+        dp[tog][i][j] += dp[!tog][i - a[0][k]][j - a[1][k]] + dp[!tog][i][j];
+      }
+    }
+  }
+  printf("%lld\n", dp[tog][w][w] - 1);
 }
--- baekjoon/1415.cpp
+++ baekjoon/1415.cpp
@@ -1,50 +1,55 @@
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
-#include <vector>
-#include <limits.h>
-#include <queue>
-#include <stack>
-#include <set>
+
 #include <algorithm>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
 using namespace std;
 #define ll long long
 const int MAX = 500000 + 1;
-bool p[MAX] = { true,true,false, };
+bool p[MAX] = {
+    true,
+    true,
+    false,
+};
 int cnt[10001];
 ll dp[2][MAX];
 
-int main()
-{
-	int s = 0, M = 0;
-	for (int i = 2; i < MAX; i++) {
-		if (p[i])continue;
-		for (int j = 2; j*i < MAX; j++) {
-			p[j*i] = true;
-		}
-	}
-	int n;
-	scanf("%d", &n);
-	for (int i = 0, v; i < n; i++) {
-		scanf("%d", &v);
-		cnt[v]++; s += v; M = max(M, v);
-	}
-	bool tog = true;
-	dp[tog][0] = cnt[0] + 1;
-	for (int i = 1; i <= M; i++) {
-		if (!cnt[i])continue;
-		tog = !tog;
-		memset(dp[tog], 0, sizeof(dp[tog]));
-		for (int j = 0; j <= cnt[i]; j++) {
-			for (int k = s; k >= i*j; k--) {
-				if (k - i*j < 0)break;
-				dp[tog][k] += dp[!tog][k - i*j];
-			}
-		}
-	}
-	ll R = 0;
-	for (int i = 2; i <= s; i++) {
-		if (!p[i])R += dp[tog][i];
-	}
-	printf("%lld\n", R);
-
+int main() {
+  int s = 0, M = 0;
+  for (int i = 2; i < MAX; i++) {
+    if (p[i]) continue;
+    for (int j = 2; j * i < MAX; j++) {
+      p[j * i] = true;
+    }
+  }
+  int n;
+  scanf("%d", &n);
+  for (int i = 0, v; i < n; i++) {
+    scanf("%d", &v);
+    cnt[v]++;
+    s += v;
+    M = max(M, v);
+  }
+  bool tog = true;
+  dp[tog][0] = cnt[0] + 1;
+  for (int i = 1; i <= M; i++) {
+    if (!cnt[i]) continue;
+    tog = !tog;
+    memset(dp[tog], 0, sizeof(dp[tog]));
+    for (int j = 0; j <= cnt[i]; j++) {
+      for (int k = s; k >= i * j; k--) {
+        if (k - i * j < 0) break;
+        dp[tog][k] += dp[!tog][k - i * j];
+      }
+    }
+  }
+  ll R = 0;
+  for (int i = 2; i <= s; i++) {
+    if (!p[i]) R += dp[tog][i];
+  }
+  printf("%lld\n", R);
 }
